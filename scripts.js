4cc0c81cde882cf9579556dc38983a53f2f23781
Dmytro <plekhotkindmytro@gmail.com>
2017-06-18 09:59:56 +0300

    Implement first working version without logos

--- scripts.js
+++ scripts.js
@@ -1,41 +1,60 @@
-var API_URL = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&callback=?&search=";
+var CHANNELS_URL = "https://wind-bow.glitch.me/twitch-api/channels/";
+var USERS_URL = "https://wind-bow.glitch.me/twitch-api/users/";
+var STREAMS_URL = "https://wind-bow.glitch.me/twitch-api/streams/";
 
-
+var USERS = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 $(document).ready(function () {
-
-    $("#search-btn").on("click", search);
-    $("#search-text").on("keyup", function (e) {
-        if (e.keyCode == 13) {
-            search();
-        }
-    });
-
-    $("#random-btn").on("click", random);
+    loadTwitchData();
 });
 
-function search() {
-    var url = API_URL + $("#search-text").val();
-    $.getJSON(url, function (json) {
-        var searchText = json[0];
-        var titles = json[1];
-        var descriptions = json[2];
-        var urls = json[3];
-        var template;
-        $("#result").html(""); // clear before append
+function loadTwitchData() {
 
-        for (var i = 0; i < titles.length; i++) {
-            template = CARD_TEMPLATE.replace("{title}", titles[i]);
-            template = template.replace("{url}", urls[i]);
-            template = template.replace("{description}", descriptions[i]);
-            $("#result").append(template);
-        }
+    USERS.forEach(function (user) {
+        $.getJSON(STREAMS_URL + user, function (json) {
+            var stream = json.stream;
+            var chanelStatus = "";
+            var chanelUrl;
+            var chanelLogo;
+            var game;
+            var displayName;
+            if (stream) {
+                chanelStatus = stream.channel.status;
+                chanelUrl = stream.channel.url;
+                chanelLogo = stream.channel.logo;
+                displayName = stream.channel.game;
+                game = stream.channel.game;
+                var template = ONLINE_CARD_TEMPLATE.replace("{url}", "https://www.twitch.tv/" + user);
+                template = template.replace("{displayName}", displayName);
+                template = template.replace("{description}", game + ": " + chanelStatus);
+                $("#result").append(template);
+
+            } else {
+
+                $.getJSON(USERS_URL + user, function (json) {
+                    var displayName = json.display_name;
+                    var logo = json.logo;
+                    var template = OFFLINE_CARD_TEMPLATE.replace("{url}", "https://www.twitch.tv/" + user);
+                    template = template.replace("{displayName}", displayName);
+                    $("#result").append(template);
+                });
+
+            }
+
+        });
 
     });
 }
 
-var CARD_TEMPLATE = '<div class="card"> \
+
+var ONLINE_CARD_TEMPLATE = '<div class="card"> \
   <div class="card-block"> \
-   <h4 class="card-title"><a href="{url}">{title}</a></h4> \
+   <h4 class="card-title"><a href="{url}">{displayName}</a></h4>(online) \
     {description} \
+  </div> \
+</div>';
+
+var OFFLINE_CARD_TEMPLATE = '<div class="card"> \
+  <div class="card-block"> \
+   <h4 class="card-title"><a href="{url}">{displayName}</a></h4>(offline) \
   </div> \
 </div>';
